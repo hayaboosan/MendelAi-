63a779830ba5bc8810a0147879cc02b7d3165e5b
hayaboosan <hayaboosan@gmail.com>
2022-02-21 19:12:31 +0900

    [Add]系統モデルとマイグレーション

--- mendel_japan/models.py
+++ mendel_japan/models.py
@@ -20,6 +20,7 @@ class Boar(db.Model):
     """雄モデル
 
     ・雄 多 : 農場 1
+    ・雄 多 : 系統 1
     """
     __tablename__ = 'boars'
 
@@ -27,9 +28,9 @@ class Boar(db.Model):
     tattoo = db.Column(db.String(50), unique=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     birth_on = db.Column(db.Date)
-    line = db.Column(db.String(50), nullable=False)
     culling_on = db.Column(db.Date)
     farm_id = db.Column(db.Integer, db.ForeignKey('farms.id'))
+    line_id = db.Column(db.Integer, db.ForeignKey('lines.id'))
 
 
 class Farm(db.Model):
@@ -58,3 +59,18 @@ class AiStation(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
     farm_ids = db.relationship('Farm', backref='ai_stations', lazy=True)
     user_ids = db.relationship('User', backref='ai_stations', lazy=True)
+
+
+class Line(db.Model):
+    """系統モデル
+
+    ・系統 1 : 雄 多
+    """
+    __tablename__ = 'lines'
+
+    id = db.Column(db.Integer, primary_key=True)
+    line = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    abbreviation = db.Column(db.String(50), unique=True, nullable=False)
+    code = db.Column(db.String(50), unique=True)
+    boar_ids = db.relationship('Boar', backref='lines', lazy=True)
