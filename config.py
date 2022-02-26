a07aeace6941b1121950b8dfaf895311c67e9ef5
hayaboosan <hayaboosan@gmail.com>
2022-02-26 13:38:31 +0900

    [Change]DB接続設定

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 import sqlalchemy
 
 load_dotenv('.env')
-DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+DATABASE_URI = os.environ.get('DATABASE_URL')
 engine: sqlalchemy.engine = \
     sqlalchemy.create_engine(DATABASE_URI, echo=True)
