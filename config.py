48f0ca9e476ee002e0550da1efe837824054c1ed
hayaboosan <hayaboosan@gmail.com>
2022-02-26 15:31:54 +0900

    DB接続設定

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 import sqlalchemy
 
 load_dotenv('.env')
-DATABASE_URI = f"postgresql+psycopg2://{os.environ.get('DATABASE_URL')}"
+DATABASE_URI = f"{os.environ.get('DATABASE_URL')}"
 engine: sqlalchemy.engine = \
     sqlalchemy.create_engine(DATABASE_URI, echo=True)
